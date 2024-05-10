694384cbe7ff9a22cd35085a4af1e38a09bf6cfb
annusinha1 <annusinha15029@gmail.com>
2024-05-10 10:19:59 +0530

    check

--- ArraySearch.c
+++ ArraySearch.c
@@ -1,17 +1,28 @@
-#include <stdio.h>
+// #include <stdio.h>
+// int main(){
+//     int arr[5]= {20, 89, 36, 548, 68};
+//     int i, a;
+//     printf("Enter the number you want to search for: ");
+//     scanf("%d", &a);
+//     for(i=0; i<5; i++){
+//         if (arr[i] == a)
+//             printf("\n Search Found");
+
+//         else
+//             printf("\n Not Found");
+
+//         break;
+//     }
+//     return 0;   
+// }
+
+
+#include<stdio.h>
 int main(){
-    int arr[5]= {20, 89, 36, 548, 68};
-    int i, a;
-    printf("Enter the number you want to search for: ");
-    scanf("%d", &a);
-    for(i=0; i<5; i++){
-        if (arr[i] == a)
-            printf("\n Search Found");
-
-        else
-            printf("\n Not Found");
-
-        break;
-    }
-    return 0;   
+    int a, b, sum;
+    printf ("Enter a two no: ");
+    scanf( "%d %d ", &a, &b);
+    sum = a+b;
+    printf(" Sum is: %d ", sum);
+    return 0 ;
 }
